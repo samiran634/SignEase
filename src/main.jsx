041950c18a0ca33799bf1e7cffc3342d24047a78
samiran <codegeeksam@gmail.com>
2025-01-19 12:23:04 +0530

    proper strature of project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { ClerkProvider } from '@clerk/clerk-react'
+const PUBLISHABLEKEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if(!PUBLISHABLEKEY)alert("please set your publishable key");
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLEKEY}>
     <App />
+    </ClerkProvider>
+  
   </StrictMode>,
 )
