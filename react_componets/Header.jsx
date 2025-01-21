ea070812208bc77437108b3cbc53c08cd011d07e
ankitkundu837 <ankitkundu837@gmail.com>
2025-01-22 01:47:56 +0530

    adding header and navbar

---
+++ react_componets/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header()
+{
+    return(
+        <header className="header">
+        <span className="welcome">Welcome</span>
+        <span className="team-name">Team Name</span>
+        </header>
+    )
+}
---
+++ react_componets/Navbar.jsx
@@ -0,0 +1,11 @@
+
+export default function Navbar()
+{
+    return(
+        <nav className="navbar">
+            <a href="/"  >Home</a>
+            <a href="/agreements" >Agreements</a>
+            <a href="/account" >Account</a>
+        </nav>
+    )
+}
