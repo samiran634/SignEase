682038a4b31f8d16aacf78f4973e0507daf6e7bb
samiran <145244691+samiran634@users.noreply.github.com>
2025-03-02 16:41:59 +0530

    small modifications

--- frontend/src/components/loccked_components/pdfComp.jsx
+++ frontend/src/components/loccked_components/pdfComp.jsx
@@ -12,7 +12,6 @@ function PdfComp({ pdfFile }) {
  }]
   return (
     <>
-
     <div className="flex w-full h-screen mt-0 mr-0 pt-8 bg-zinc-50 overflow-auto">
       <DocViewer documents={docs} pluginRenderers={DocViewerRenderers}
         style={{height:1000,color:"black"}}
